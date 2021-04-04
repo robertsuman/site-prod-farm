307cf267a1fb0ec6be8f10c67b7f7caa9892cc98
Robert Suman <robert@nextar.com.br>
2021-04-04 18:33:20 -0300

    ajustando index.js 6

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.static(__dirname +'/public'));
 app.use(express.static('public'));
 app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.get('/', (req, res) => {
 //     res.sendFile('index.html', {root: __dirname + '/public/'});
 // });
