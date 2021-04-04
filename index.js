0a5ca9412824ff5ca36862436b5a1a7bfd49b563
Robert Suman <robert@nextar.com.br>
2021-04-04 18:29:05 -0300

    ajustando index.js 6

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var express = require('express');
 var app = express();
 // Render static files
 app.use(express.static(__dirname +'/public'));
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + '/public/'});
-});
+app.use(express.static('public'));
+app.use(express.static('/public'));
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html', {root: __dirname + '/public/'});
+// });
 // Port website will run on
 app.listen(process.env.PORT || 8080)
