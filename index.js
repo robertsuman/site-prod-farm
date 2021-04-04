80809555fd27e2cb59923b33d31b6d338bb797ee
Robert Suman <robert@nextar.com.br>
2021-04-04 17:22:44 -0300

    ajustando index.js 3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 // Initialise Express
 var app = express();
 // Render static files
+app.use(express.static('web'));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: '/web/'});
 });
