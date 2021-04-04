143e519ef555b46c43b8771453d00410a9387605
Robert Suman <36929285+robertsuman@users.noreply.github.com>
2021-04-04 19:46:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 // Load Node modules
 var express = require('express');
-var path = require('path');
 // Initialise Express
 var app = express();
+
 // Render static files
-app.use(express.static(__dirname +'/public'));
-app.use(express.static('public'));
-app.use(express.static('/public'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '/public')));
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html', {root: __dirname + '/public/'});
-// });
+app.use(express.static('web'));
+
 // Port website will run on
 app.listen(process.env.PORT || 8080)
